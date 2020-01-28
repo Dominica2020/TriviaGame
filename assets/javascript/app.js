ebc4899adb6e98e7ce14be3537ea246ee10124f5
Gecko Uno <dominica.patterson@gmail.com>
2020-01-27 19:48:12 -0800

    more updates to homework, start button on click now reveals quiz div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,39 @@
-//----------To Do---------//
+/* ----------To Do---------
+1: On start only containter one is visible
+2: On start button click reveal second container and stat timer countdown
+3. Create multiple choice quiz where only one anser can be selected
+4. On timer ending end quiz and reveal results
+5. Reset quiz
+*/
 
-//Click Start button to start timer countdown and reveal the trivia questions
-//Only 1 answer per question is to be allowed
-//Once countdown runs out game ends and results are displayed (how many correct & how many wrong)
+//Start Game Function//
+function startButton() {
+    $("#quiz").css("display", "block");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  /*
+  function startFunction() {
+    var quiz = document.getElementById("quiz");
+    if (quiz.style.display === "none") {
+      quiz.style.display = "block";
+    } else {
+      quiz.style.display = "none";
+    }
+  }
+
+
+  function myFunction() {
+    document.getElementById("#quiz").style.visibility = "hidden";
+}
+  */
